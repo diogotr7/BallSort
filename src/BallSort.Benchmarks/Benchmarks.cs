d90b19d78444b68df5d826e81673596ff031b46b
Diogo Trindade <diogotr7@gmail.com>
2023-10-06 13:58:08 +0100

    add benchmarks of diff sizes

--- src/BallSort.Benchmarks/Benchmarks.cs
+++ src/BallSort.Benchmarks/Benchmarks.cs
@@ -7,12 +7,19 @@ namespace BallSort.Benchmarks;
 [ShortRunJob]
 public class Benchmarks
 {
-    private readonly Puzzle _puzzle = Puzzle.CreateRandom(new GameSettings(8, 2, 4), 0);
+    private readonly Puzzle[] _puzzles = {
+        Puzzle.CreateRandom(new GameSettings(8, 2, 4), 0),
+        Puzzle.CreateRandom(new GameSettings(10, 2, 4), 0),
+        Puzzle.CreateRandom(new GameSettings(14, 2, 4), 0),
+    };
+    
+    [Params(0, 1, 2)]
+    public int PuzzleIndex { get; set; }
     
     [Benchmark]
     public void Solve()
     {
-        var game = new Solver(_puzzle);
+        var game = new Solver(_puzzles[PuzzleIndex]);
         game.solve_single();
     }
 }
